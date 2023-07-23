9f6322fa5ee6a089a9d674cafba34189c900083e
gerbertwest <gerbert.west@gmail.com>
2023-07-23 18:54:28 +0300

    2.10. Создаём проекты, готовим REST (часть 3)

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,10 +1,40 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserCity } from "@project/shared/app-types";
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User birth date',
+    example: '1981-03-12',
+  })
   public dateBirth: string;
+
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks',
+  })
   public firstname: string;
+
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Ivanov'
+  })
   public lastname: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456'
+  })
   public password: string;
+
+  @ApiProperty({
+    description: 'User city',
+    example: 'Санкт-Петербург'
+  })
   public city: UserCity;
 }
