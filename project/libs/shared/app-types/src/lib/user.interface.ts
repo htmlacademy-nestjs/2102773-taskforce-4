95a0b30cbaf8221306642bb23a0c7474c5c7b912
gerbertwest <gerbert.west@gmail.com>
2023-07-23 12:18:36 +0300

    2.10. Создаём проекты, готовим REST (часть 1)

---
+++ project/libs/shared/app-types/src/lib/user.interface.ts
@@ -0,0 +1,14 @@
+import { UserCity } from './user-city.enum';
+import {UserRole} from './user-role.enum';
+
+export interface User {
+  _id?: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  dateBirth: Date;
+  avatar: string;
+  passwordHash: string;
+  role: UserRole;
+  city: UserCity;
+}
